16f047e99e798eee076142b10415d3f9bda75343
Anzhela53 <a.y.golikova@gmail.com>
2023-11-09 22:54:18 +0300

    add: add x variable in localStorage

--- public/vendor/app.js
+++ public/vendor/app.js
@@ -1,4 +1,9 @@
 function something()
 {
-    alert('Hello!');
+    var x = window.localStorage.getItem('aaa');
+    x = x * 1 + 1;
+
+    window.localStorage.setItem('aaa', 555);
+    alert(x);
+
 }
