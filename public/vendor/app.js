6f9067105b25bdec3c0ed67ba5a504f4e2d21321
Anzhela53 <a.y.golikova@gmail.com>
2023-11-11 09:20:30 +0300

    fix: change the path to the file app.js

--- public/vendor/app.js
+++ public/vendor/app.js
@@ -3,7 +3,7 @@ function something()
     var x = window.localStorage.getItem('aaa');
     x = x * 1 + 1;
 
-    window.localStorage.setItem('aaa', 555);
+    window.localStorage.setItem('aaa', x);
     alert(x);
 
 }
